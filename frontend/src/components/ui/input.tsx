b61597fb9073f20f691dff7a9dab5450bcf7db4b
Muhammad Kamran <mkrnaqeebi@gmail.com>
2025-01-29 23:53:39 +0000

    ✨ Add frontend configuration files, API types, and utility functions

--- frontend/src/components/ui/input.tsx
+++
@@ -1,7 +0,0 @@
-const GenInput = ({name: string}) => {
-    return <>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name={name} />
-    </>
-}
-export default GenInput;
